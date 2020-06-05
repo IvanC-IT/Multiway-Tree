fba5775a1bb717c136e58f1bf4bd4d53a0f409f0
IvanC-IT <54840154+IvanC-IT@users.noreply.github.com>
2020-06-05 02:36:30 +0200

    Update draw_tree_multiway.py

--- draw_tree_multiway.py
+++ draw_tree_multiway.py
@@ -1,10 +1,7 @@
-# - * - coding: utf-8 - * -
 class Tree:
     def __init__(self,V):
         self.id=V
         self.f=[]
-
-
 
 def fromLista1(lista):
     '''Create tree from a list [value, childlist]
@@ -16,58 +13,58 @@ def toLista(node):
     ''' Convert the tree to a list of lists [value, childlist]'''
     return [node.id, [toLista(x) for x in node.f]] 
 
-def convert(root):
+def converti(root):
     if not root.f:
-        #      string     c  w  h
+        #      stringa    c  w  h
         return [root.id], 0, 2, 1
     N = len(root.f)
-    subtrees = [convert (son) for son in root.f]
-    # if 1
-    rows, c, w, H = subtrees [0]
-    if N == 1:
-        first_line = '' * c + root.id + '' * (w-c-2)
-        second_line = '' * c + '|' + '' * (w-c-1)
-        rows [0: 0] = [first_line, second_line]
-        return rows, c, w, H + 2
-    # if N
-    pad(subtrees)
-    rows, A, B, W, H = concatenate(subtrees)
-    center = (A + B) // 2
-    row1 = '' * center + root.id + '' * (W-center-2)
-    row2 = '' * (A + 1) + '_' * (center-A-1) + '|' + '_' * (B-center-1) + '' * (W-B)
-    row3 = '' .join (['' * c + '|' + '' * (w-c-1) for _, c, w, _ in subtrees])
-    rows[0:0] = [row1, row2, row3]
-    return rows, center, W, H + 3
+    sottoalberi = [ converti(son) for son in root.f ]
+    # se 1
+    righe, c, w, H = sottoalberi[0]
+    if N==1:
+        prima_riga   = ' '*c + root.id + ' '*(w-c-2)
+        seconda_riga = ' '*c + '|'     + ' '*(w-c-1)
+        righe[0:0] = [prima_riga, seconda_riga]
+        return righe, c, w, H+2
+    # se N
+    pad(sottoalberi)
+    righe, A, B, W, H = concatena(sottoalberi)
+    centro = (A+B)//2
+    riga1 = ' '*centro + root.id + ' '*(W-centro-2)
+    riga2 = ' '*(A+1) + '_'*(centro-A-1) + '|' + '_'*(B-centro-1) + ' '*(W-B)
+    riga3 = '  '.join([ ' '*c + '|'+' '*(w-c-1) for _,c,w,_ in sottoalberi ] )
+    righe[0:0] = [ riga1, riga2, riga3 ]
+    return righe, centro, W, H+3
 
-def concatenate(subtrees):
-    '' 'concatenate rows of subtrees of the same number of rows by entering 2 spaces' ''
-    texts, a, W, h = subtrees [0]
+def concatena(sottoalberi):
+    testi, a, W, h = sottoalberi[0]
     b = a
-    for lines, c, w, h1 in subtrees [1:]:
-        b = W + c + 2
+    for righe, c, w, h1 in sottoalberi[1:]:
+        b  = W + c + 2
         W += w + 2
-        for i,row in enumerate(rows):
-            texts [i] += '' + row
-    return texts, a, b, W, h
+        for i, riga in enumerate(righe):
+            testi[i] += '  ' + riga
+    return testi, a, b, W, h
 
-def pad(subtrees):
+def pad(sottoalberi):
     H = 0
-    for lines, _c, _w, _h in subtrees:
-        H = max (H, len (lines))
-    for lines, _c, w, _h in subtrees:
-        h = len (lines)
-        if h <H:
-            pad = '' * w
-            rows.extend ([pad] * (H-h))
+    for righe, _c, _w, _h in sottoalberi:
+        H = max(H,len(righe))
+    for righe, _c,  w, _h in sottoalberi:
+        h = len(righe)
+        if h < H:
+            pad = ' '*w
+            righe.extend([pad]*(H-h))
 
 def es2(r):
-    #insert your code here
-    lines, c, w, h = convert (r)
-    return '\ n'.join (lines)
-
+    #inserisci qui il tuo codice
+    righe, c, w, h = converti(r)
+    return '\n'.join(righe)
+       
 
 if __name__ == '__main__':
+
     # enter your personal test instructions here
-    list = ['100', [['02', [['01', []]]], ['10', [['01', []], ['02', [['03' , []], ['06', []]]], ['09', []], ['08', []], ['30', []]]], ['06', []]]]
-    r = fromLista1 (list)
+    lista = ['100', [['02', [['01', []]]], ['10', [['01', []], ['02', [['03' , []], ['06', []]]], ['09', []], ['08', []], ['30', []]]], ['06', []]]]
+    r = fromLista1 (lista)
     print (es2 (r))
